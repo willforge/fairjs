00e45e3c4ebbf79787acd4ea5cdbae8c311c8e0a
Rufus J. Fickas <rfickas@gctm.ml>
2021-02-15 19:39:56 +0100

    fixed error in ipns cache

--- ipfs.js
+++ ipfs.js
@@ -22,8 +22,8 @@ const qmNull = 'QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n';
 var promisedPeerId;
 var ipns_cache = {
   '12D3KooWHpj1ercuLscv78Ezz4TADdwSgNkmNmd4qH8dfCCcwZHX':'/ipfs/QmU2vXnWUHmyDygqjsEJDxm7NkCE943CTqZSTq4wrE111M',
-  'QmTeqJutKAtVyX39qvhAGfjQFesbubamN8dvVPMg5jYRwS':'/ipfs/QmeihygynRMSLKZNEhQ2zavyRGKXjH2YGhyERokE3Lf6u1'
-  '12D3KooWJDBrt6re8zveUPZKwC3QPBid4iCguyMVuWbKMXb5HeTa':'/ipfs/bafyaabakaieac',
+  'QmTeqJutKAtVyX39qvhAGfjQFesbubamN8dvVPMg5jYRwS':'/ipfs/QmeihygynRMSLKZNEhQ2zavyRGKXjH2YGhyERokE3Lf6u1',
+  '12D3KooWJDBrt6re8zveUPZKwC3QPBid4iCguyMVuWbKMXb5HeTa':'/ipfs/bafyaabakaieac'
   };
 
 
