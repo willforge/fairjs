4f6cbfae4791e84d9038c830329370a51171cd84
Velma D. Daughrity <vdaughrity@clockchain.tk>
2021-02-15 19:33:16 +0100

    removed config.js and add initial value for ipns_cache

--- ipfs.js
+++ ipfs.js
@@ -20,7 +20,11 @@ const qmNull = 'QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n';
 //const cfg_url = 'http://127.0.0.1:1124/config.json';
 //var promisedConfig = load_config(cfg_url);
 var promisedPeerId;
-var ipns_cache = {};
+var ipns_cache = {
+  '12D3KooWHpj1ercuLscv78Ezz4TADdwSgNkmNmd4qH8dfCCcwZHX':'/ipfs/QmU2vXnWUHmyDygqjsEJDxm7NkCE943CTqZSTq4wrE111M',
+  'QmTeqJutKAtVyX39qvhAGfjQFesbubamN8dvVPMg5jYRwS':'/ipfs/QmeihygynRMSLKZNEhQ2zavyRGKXjH2YGhyERokE3Lf6u1'
+  '12D3KooWJDBrt6re8zveUPZKwC3QPBid4iCguyMVuWbKMXb5HeTa':'/ipfs/bafyaabakaieac',
+  };
 
 
 var thisscript = document.currentScript;
