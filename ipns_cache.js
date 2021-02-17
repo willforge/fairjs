36394ff6c1f29f50e3ae76b4edd1dbdc299ae3d4
Elias J. Baffa <ebaffa@dids.ml>
2021-02-17 17:18:22 +0100

    changed alain's root cid encoding (was bafyaa...)

--- ipns_cache.js
+++ ipns_cache.js
@@ -3,5 +3,7 @@
 window.ipns_cache = {
   '12D3KooWHpj1ercuLscv78Ezz4TADdwSgNkmNmd4qH8dfCCcwZHX':'/ipfs/QmU2vXnWUHmyDygqjsEJDxm7NkCE943CTqZSTq4wrE111M',
   'QmTeqJutKAtVyX39qvhAGfjQFesbubamN8dvVPMg5jYRwS':'/ipfs/QmeihygynRMSLKZNEhQ2zavyRGKXjH2YGhyERokE3Lf6u1',
-  '12D3KooWJDBrt6re8zveUPZKwC3QPBid4iCguyMVuWbKMXb5HeTa':'/ipfs/bafyaabakaieac'
+  /* 'alain':'/ipfs/QmVj21ku1uxpnAYdSNwjK7vQPbKE5vZhRciVhbpDmafp6h', */
+  '12D3KooWJDBrt6re8zveUPZKwC3QPBid4iCguyMVuWbKMXb5HeTa':'/ipfs/QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn',
+  'QmezgbyqFCEybpSxCtGNxfRD9uDxC53aNv5PfhB3fGUhJZ':'/ipfs/bafyaabakaieac'
 };
