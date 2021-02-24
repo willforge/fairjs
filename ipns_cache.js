7b60fa575abd3966a2b250f253f3902ce5ef5e02
Juliette D. Lecaros <jlecaros@bigtm.gq>
2021-02-24 16:42:48 +0100

    use a default public for ipns_cache

--- ipns_cache.js
+++ ipns_cache.js
@@ -1,10 +1,11 @@
 /* ipns cache file to speedup name resolution */
 
 window.ipns_cache = {
-  '12D3KooWHpj1ercuLscv78Ezz4TADdwSgNkmNmd4qH8dfCCcwZHX':'/ipfs/QmU2vXnWUHmyDygqjsEJDxm7NkCE943CTqZSTq4wrE111M',
-  'QmTeqJutKAtVyX39qvhAGfjQFesbubamN8dvVPMg5jYRwS':'/ipfs/QmeihygynRMSLKZNEhQ2zavyRGKXjH2YGhyERokE3Lf6u1',
-  'QmcfHufAK9ErQ9ZKJF7YX68KntYYBJngkGDoVKcZEJyRve':'/ipfs/QmaitRWPF2vnPUusJTM9u5WpnGds1viYvVepezXEXyHSqz',
-  '12D3KooWJDBrt6re8zveUPZKwC3QPBid4iCguyMVuWbKMXb5HeTa':'/ipfs/QmV2vFKermkS2hdUHhbKXc4rS41zeoEG4zzALHUFjzTVr9',
+  // use a "generic" public directory to kickstart ipns
+  '12D3KooWHpj1ercuLscv78Ezz4TADdwSgNkmNmd4qH8dfCCcwZHX':'/ipfs/QmTc2ZGJfPUocFXQ9euS9X7uHjW1CovasHTZGrUW4KPo8Y',
+  'QmTeqJutKAtVyX39qvhAGfjQFesbubamN8dvVPMg5jYRwS':'/ipfs/QmTc2ZGJfPUocFXQ9euS9X7uHjW1CovasHTZGrUW4KPo8Y',
+  'QmcfHufAK9ErQ9ZKJF7YX68KntYYBJngkGDoVKcZEJyRve':'/ipfs/QmTc2ZGJfPUocFXQ9euS9X7uHjW1CovasHTZGrUW4KPo8Y',
+  '12D3KooWJDBrt6re8zveUPZKwC3QPBid4iCguyMVuWbKMXb5HeTa':'/ipfs/QmTc2ZGJfPUocFXQ9euS9X7uHjW1CovasHTZGrUW4KPo8Y',
   'QmAnon': 'QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn', // anonymous
   'QmezgbyqFCEybpSxCtGNxfRD9uDxC53aNv5PfhB3fGUhJZ':'/ipfs/bafyaabakaieac' // public
 };
